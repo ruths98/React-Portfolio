b3f22a724072000a1e591079874b45670f051dd1
ruths98 <ruth91198@gmail.com>
2023-06-24 14:09:49 -0400

    troubleshooting pushing

---
+++ react-portfolio/src/components/resume.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Resume(){
+
+    return(
+        <div>
+           <h3>Resume</h3>
+
+        <button>Download </button>
+        </div>
+      )
+}
